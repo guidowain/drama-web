86854f73fc187d1ba7a491bba9a285eb9e83bb21
Guido Wain <guido.wainstein@gmail.com>
2026-04-29 15:07:46 -0300

    auto deploy 2026-04-29 15:07:46

--- next.config.js
+++ next.config.js
@@ -3,6 +3,25 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/miamigayyo',
+        destination: 'https://programademano.com.ar/miamigayyo',
+        permanent: true,
+      },
+      {
+        source: '/lacenadelostontos',
+        destination: 'https://programademano.com.ar/lacenadelostontos',
+        permanent: true,
+      },
+      {
+        source: '/alejandra',
+        destination: 'https://programademano.com.ar/alejandra',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
